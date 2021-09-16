e07f88beb1e3d8997289cfdf57ea2d2fe5e9b91c
ArisGoi <aris.goi97@gmail.com>
2021-09-16 14:10:07 +0200

    add image

--- main.js
+++ main.js
@@ -7,6 +7,11 @@
 const app = new Vue({
     el: '#root',
     data: {
-      saluto: 'Hello Vue!'
+      saluto: 'Hello Vue!',
+
+      image: {
+          url:'https://2.bp.blogspot.com/-F4jszpW691M/WC91ORuwsoI/AAAAAAAAB-c/rtFnTYmQRLAdv4ThqRYj8qLoWmtEcg6egCLcB/s1600/Animated-GIF-Banana.gif',
+          alt: 'banana che balla'
+      }
     }
   })
